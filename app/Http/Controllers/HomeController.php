9d0113f00e58ce31b14fd2af722b82eae938cc4e
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-08 15:57:25 +0100

    finito? finito.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Movie;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +12,6 @@ class HomeController extends Controller
     public function index(){
 
         $movies = Movie::all();
-        // dd($movies);
-        return view('home', $movies);
+        return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
     <div class="container">
-        <h1>HOME AAAAAAAAAAAA</h1>
+        @foreach($movies as $movie)
+            <div class="card">
+                <h2>{{$movie->title}}</h2>
+                <h3>{{$movie->original_title}}</h3>
+                <h4>vote: {{$movie->vote}}</h4>
+            </div>
+        @endforeach
     </div>
-
 @endsection
